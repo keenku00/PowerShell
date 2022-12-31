77ae52988d701568e3e9c346bf99928d4f1c7ad2
JJimmy__jib <46073026+keenku00@users.noreply.github.com>
2022-12-31 21:39:12 +0900

    [Sys]_Mstsc-Access-Check.ps1
    [Reference:https://jpcertcc.github.io/ToolAnalysisResultSheet/details/mstsc.htm]
    Destination Host
    RDP session connection start/end time and date, source host IP address, logged-in user name and account domain, and success or failure connection (Microsoft-Windows-TerminalServices-LocalSessionManager/Operational, Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational log)

---
+++ Scripts/[Sys]_Mstsc-Access-Check.ps1
@@ -0,0 +1,45 @@
+<#
+	.DESCRIPTION
+		To check who accessed to the server directly via terminal access.
+	
+	.PARAMETER
+
+	.NOTES
+		===========================================================================
+		Created on:   	3rd Sep., 2022
+		Created by:   	Jimmy Lee
+		Contact to:   	keenku00@naver.com
+		Filename:     	[Sys]_Mstsc-Access-Check.ps1
+		===========================================================================		
+
+    .Configuration Mnagement
+    2022.09.03
+        : Script creation
+    2022.09.03
+        : UAT done
+    2022.12.31
+        : Upload in Git.
+#>
+
+<#
+[When someone accessed to the server via MSTSC or terminal session, the log data should be recorded]
+#>
+
+Get-Date
+Get-Date | Get-Member
+$today = Get-Date
+$todayDetails = $today.ToString("yyyy-MM-dd_HH-mm")
+
+(Get-Date).AddDays(-2)
+$today = Get-Date
+$Days_defined = $Today.AddDays(-2)
+
+$folderpath = "$env:USERPROFILE\"
+$destination = "$env:USERPROFILE\Desktop\$todayDetails.txt"
+$Width = 1024
+
+Get-WinEvent -LogName 'Microsoft-Windows-TerminalServices-LocalSessionManager/Operational' -MaxEvents 300 |
+Select-Object TimeCreated, ID, ProviderName, LevelDisplayName, Message |
+Where-Object {($_.TimeCreated -le $today) -or ($_.TimeCreated -ge $today)} |
+Where-Object{$_.TimeCreated -gt $Days_defined} |
+Out-file -FilePath $Destination
