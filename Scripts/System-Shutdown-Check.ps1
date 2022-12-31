f85ca22a0e0f2cba2c0b09b6e8f756f9894239f1
JJimmy__jib <46073026+keenku00@users.noreply.github.com>
2022-12-31 14:54:12 +0900

    System-Shutdown-Check.ps1

---
+++ Scripts/System-Shutdown-Check.ps1
@@ -0,0 +1,45 @@
+<#
+	.DESCRIPTION
+		To check who turn off the Windows server, the script is created. 
+	
+	.PARAMETER
+
+	.NOTES
+		===========================================================================
+		Created on:   	3rd Sep., 2022
+		Created by:   	Jimmy Lee
+		Contact to:   	jimmy.lee@daimler.com  
+		Corp. Info: 	  Mercedes-Benz Truck(Daimler Trucks Korea Ltd.),
+		Filename:     	TBD
+		===========================================================================		
+
+    .Configuration Mnagement
+    2022.09.03
+        : Script creation
+    2022.09.03
+        : UAT done
+#>
+
+<#
+[When execute the script taking much time, there're a few preparations we need to take care on the server itself.]
+
+** Extract the shutdown log data from 1 Year ago as of now.
+#>
+
+Get-Date
+Get-Date | Get-Member
+$today = Get-Date
+$todayDetails = $today.ToString("yyyy-MM-dd_HH-mm")
+
+(Get-Date).AddDays(-366)
+$today = Get-Date
+$Days_defined = $Today.AddDays(-366)
+
+Get-EventLog -LogName system -Source user32 |
+Where-Object{($_.TimeGenerated -ge $Days_defined)} | fl * 
+
+<#
+[Extract the log data when the system was off by which account]
+#>
+
+Get-WinEvent -filterhash @{Logname = 'system';ID=1074} -MaxEvents 1000 | Select-Object @{Name="Computername";Expression = {$_.machinename}}, @{Name="UserName";Expression = {$_.UserId.translate([System.Security.Principal.NTAccount]).value}}, TimeCreated
