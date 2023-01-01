dd421e6fb047261f557967508da4d4d7bfafa0d3
JJimmy__jib <46073026+keenku00@users.noreply.github.com>
2023-01-01 18:42:32 +0900

    [Remote]_PSSession-Connection-Execution.ps1

---
+++ Scripts/[Remote]_PSSession-Connection-Execution.ps1
@@ -0,0 +1,40 @@
+<#
+	.DESCRIPTION
+		Even the legacy files would be moved into the other path, the attribute value would be same as previous one. In order to change the value not to make it removed next time, the value should be changed as the new one.
+	
+	.PARAMETER
+
+	.NOTES
+		===========================================================================
+		Created on:   	8th Nov., 2022
+		Created by:   	Jimmy Lee
+		Contact to:   	keenku00@naver.com
+		Filename:     	[Remote]_PSSession-Connection-Execution.ps1
+		===========================================================================		
+
+    .Configuration Mnagement
+    2022.11.08
+        : Script creation
+    2022.11.08
+        : UAT done
+    2023.01.01
+        : Upload in Git
+#>
+
+#Input the user ID which's already having the administrative priviliage at the target server.
+$User = ""
+
+#Input the password coressponding to the user's ID above.
+$PWord = "" 
+
+#Input the target system's hostname.
+$target_hostname = ""
+
+#Make the password encrypted
+$PWord_secured = ConvertTo-SecureString -String $PWord -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord_secured
+
+#Remove the existing variable set as plain text.
+Remove-Variable $PWord
+
+Enter-PSSession -ComputerName $target_hostname -Port 5985 -Credential $Credential
