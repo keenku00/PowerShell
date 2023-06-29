c6878eb92271b34f97a71b7f250366877a8097b1
JJimmy__jib <46073026+keenku00@users.noreply.github.com>
2023-06-29 17:23:42 +0900

    Update [File-Security]_NTFS-Permission-Execution.ps1

--- Scripts/[File-Security]_NTFS-Permission-Execution.ps1
+++ Scripts/[File-Security]_NTFS-Permission-Execution.ps1
@@ -20,6 +20,9 @@
         : Upload in Git
 #>
 
+######################################################################################
+#Option#2. Modify the groups for each folder
+######################################################################################
 
 # Define the path to the subfolder
 $folderPath = "folder_path"
@@ -39,4 +42,36 @@ $permissions = @{
 foreach ($account in $permissions.Keys) {
     $permission = $permissions[$account]
     icacls $folderPath /grant "${account}:$permission"
+}
+
+######################################################################################
+#Option#1. Modify the groups at once
+######################################################################################
+# Define the path to the text file containing folder paths
+$filePath = "folder_paths.txt"
+
+# Read the folder paths from the text file
+$folderPaths = Get-Content $filePath
+
+# Define the permissions for different accounts
+$permissions = @{
+    "NT AUTHORITY\SYSTEM"            = "(OI)(CI)F"
+    "BUILTIN\Administrators"         = "(OI)(CI)F"
+    "DOMAIN\IT-Admin-group-account"  = "(OI)(CI)F"
+    "Domain\AD-Group-needed"         = "(OI)(CI)M"
+}
+
+# Iterate over each folder path
+foreach ($folderPath in $folderPaths) {
+    # Trim any leading or trailing white spaces from the folder path
+    $folderPath = $folderPath.Trim()
+
+    # Disconnect inheritance and remove existing inherited permissions
+    icacls $folderPath /inheritance:r
+
+    # Apply the permissions to the folder
+    foreach ($account in $permissions.Keys) {
+        $permission = $permissions[$account]
+        icacls $folderPath /grant "${account}:$permission"
+    }
 }
