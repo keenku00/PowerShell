178ba434d49dec076b21d16bb7f35b94b8908a9e
JJimmy__jib <46073026+keenku00@users.noreply.github.com>
2023-07-06 00:42:37 +0900

    Update [AD]_Extract-AD-Group-Members-with-REGEX_2.ps1

--- Scripts/[AD]_Extract-AD-Group-Members-with-REGEX_2.ps1
+++ Scripts/[AD]_Extract-AD-Group-Members-with-REGEX_2.ps1
@@ -21,12 +21,35 @@
         : Upload in Git
 #>
 
-#Condition_1
-$All_K_Group = Get-ADGroup -Filter * -SearchBase "OU=A191, DC=apac, DC=corpdir, DC=net" -Properties *
+<#
+	.DESCRIPTION
+		Extract & collect the members' names belonging to the AD group defined by specific naming rules. 
+	
+	.PARAMETER
+
+	.NOTES
+		===========================================================================
+		Created on:   	5th Jul. 2023
+		Created by:   	Jimmy Lee
+		Contact to:   	keenku00@naver.com
+		Filename:     	[AD]_Extract-AD-Group-Members-with-REGEX_2.ps1
+		===========================================================================		
+
+    .Configuration Mnagement
+    2023.07.05
+        : Script creation
+    2023.07.05
+        : UAT done
+    2023.07.05
+        : Upload in Git
+#>
+
+#Condition_2
+$All_2_Group = Get-ADGroup -Filter * -SearchBase "OU=A191, DC=apac, DC=corpdir, DC=net" -Properties *
 $regex2 = 'A191_K-'
 $groupData = @()
 
-foreach ($group in $All_K_Group) {
+foreach ($group in $All_2_Group) {
     if ($group.Name -match $regex2) {
         Write-Host $group.Name
         $groupMembers = Get-ADGroupMember -Identity $group.Name | Select-Object Name
@@ -37,16 +60,16 @@ foreach ($group in $All_K_Group) {
     }
 }
 
-$groupData | Export-Csv -Path "$regex2_AD_Groups_Information.csv" -NoTypeInformation
+$groupData | Export-Csv -Path "AD_Groups_Information_2.csv" -NoTypeInformation
 
 
-#Condition_2
-$All_K_Group = Get-ADGroup -Filter * -SearchBase "OU=A191, DC=apac, DC=corpdir, DC=net" -Properties *
+#Condition_1
+$All_1_Group = Get-ADGroup -Filter * -SearchBase "OU=A191, DC=apac, DC=corpdir, DC=net" -Properties *
 $regex1 = 'A191_O-'
 $groupData = @()
 
-foreach ($group in $All_K_Group) {
-    if ($group.Name -match $regex2) {
+foreach ($group in $All_1_Group) {
+    if ($group.Name -match $regex1) {
         Write-Host $group.Name
         $groupMembers = Get-ADGroupMember -Identity $group.Name | Select-Object Name
         $groupData += New-Object -TypeName PSObject -Property @{
@@ -56,4 +79,4 @@ foreach ($group in $All_K_Group) {
     }
 }
 
-$groupData | Export-Csv -Path "$regex1_AD_Groups_Information.csv" -NoTypeInformation
+$groupData | Export-Csv -Path "1_AD_Groups_Information.csv" -NoTypeInformation
