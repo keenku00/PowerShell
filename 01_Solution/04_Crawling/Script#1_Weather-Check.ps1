9350de71805a303f3196c4a5c023396a32f0ecf5
JJimmy__jib <46073026+keenku00@users.noreply.github.com>
2023-09-05 18:23:54 +0900

    Create Script#1_Weather-Check.ps1

---
+++ 01_Solution/04_Crawling/Script#1_Weather-Check.ps1
@@ -0,0 +1,31 @@
+# Define the path to the HtmlAgilityPack.dll file (replace with the actual path)
+$htmlAgilityPackPath = "C:\Windows\System32\HtmlAgilityPack.dll"
+
+# Load the assembly
+Add-Type -Path $htmlAgilityPackPath
+
+# Define the URL of the website you want to crawl
+$url = "https://hello-bryan.tistory.com/365"
+
+# Send an HTTP request to the URL
+$response = Invoke-WebRequest -Uri $url
+
+# Check if the request was successful
+if ($response.StatusCode -eq 200) {
+    # Load the HTML content using HtmlAgilityPack
+    $html = New-Object HtmlAgilityPack.HtmlDocument
+    $html.LoadHtml($response.Content)
+
+    # Now you can traverse and extract data from the HTML
+    # For example, let's extract all the links on the page
+    $links = $html.DocumentNode.SelectNodes("//a[@href]")
+
+    if ($links -ne $null) {
+        foreach ($link in $links) {
+            $href = $link.GetAttributeValue("href", "")
+            Write-Host "Link: $href"
+        }
+    }
+} else {
+    Write-Host "Failed to retrieve the webpage. HTTP status code: $($response.StatusCode)"
+}
